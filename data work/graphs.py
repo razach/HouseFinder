f063a8239bcfafc17045d3be8e1a8fa67eeae6aa
Rohit Zacharia <razach@gmail.com>
2024-01-21 15:24:08 -0500

    Got a lot of datawork done!

---
+++ data work/graphs.py
@@ -0,0 +1,54 @@
+# %%
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import sqlalchemy as db
+
+# %%
+
+engine = db.create_engine('sqlite:///rentals.db')
+
+conn = engine.connect()
+metadata = db.MetaData()
+
+# %%
+
+# Read in rentals.db SQLLite into a pandas dataframe
+rentals_base_table = pd.read_sql_table('rentals', conn)
+
+# %%
+rentals_base_table.head()
+
+# %%
+            
+# plot average list_price with basements = 1 and basements = 0
+
+# Extract data for plotting
+basement_data = rentals_base_table[['basement', 'list_price']]
+
+# Plot scatter plot  
+
+plt.scatter(basement_data['basement'], basement_data['list_price'])
+
+
+plt.xlabel('Basement')
+plt.ylabel('List Price')
+plt.title('List Price vs Basement')
+
+plt.show()
+
+# %%
+
+# create a scatter plot of list_price vs. sqft
+
+# Extract data for plotting
+sqft_data = rentals_base_table[['sqft', 'list_price']]
+
+# Plot scatter plot
+
+plt.scatter(sqft_data['sqft'], sqft_data['list_price'])
+plt.xlabel('sqft')
+plt.ylabel('List Price')
+plt.title('Sqft vs. List Price')
+
+plt.show()
